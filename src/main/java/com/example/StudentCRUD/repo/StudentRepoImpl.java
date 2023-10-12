a58e43f5a81c5a639976d936b2389c755fbdc651
Chinmayeetingne <rashitingne12@gmail.com>
2023-10-12 17:03:58 +0530

    new files added

---
+++ src/main/java/com/example/StudentCRUD/repo/StudentRepoImpl.java
@@ -0,0 +1,43 @@
+package com.example.StudentCRUD.repo;
+
+import com.example.StudentCRUD.model.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class StudentRepoImpl implements StudentRepo{
+    private Map<Integer,Student> studentMap = new HashMap<>();
+
+    @Override
+    public Student insertStudent(Student student) {
+        studentMap.put(student.getRollNumber(),student);
+        return student;
+    }
+
+    @Override
+    public Student getStudent(int rollNumber) {
+      return   studentMap.get(rollNumber);
+    }
+
+    @Override
+    public List<Student> getStudentList() {
+        ArrayList<Student> students = new ArrayList<Student>();
+     for (Integer n :studentMap.keySet()){
+         students.add(studentMap.get(n));
+     }
+     return students;
+    }
+
+    @Override
+    public Student updateStudent(int studentId , Student student) {
+        if (studentMap.containsKey(studentId)){
+            studentMap.put(studentId, student);
+            return student;
+        }
+        return null;
+    }
+}
